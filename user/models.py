286220327348ee751a76998eab83d2630b9d7459
shalin <shalin.rono@student.moringaschool.com>
2021-10-30 11:13:03 +0300

    create user fields

--- user/models.py
+++ user/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.fields import TextField
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = CloudinaryField('profile-photo',null=True, transformation=[{'width':300, 'height':300}])
+    bio = models.TextField(null=True)
+    # neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
