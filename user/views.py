d7e5ecbdc81ea09d0fe46a15f61c71ea2a3bb7d7
shalin <shalin.rono@student.moringaschool.com>
2021-10-30 12:46:51 +0300

    create base template file

--- user/views.py
+++ user/views.py
@@ -15,7 +15,7 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegistrationForm()
-        return render(request,'users/register.html',{'form':form})
+        return render(request,'register.html',{'form':form})
 @login_required
 def profile(request):
     try:
